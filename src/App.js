fd0a513e234e0a5178bc1070af78cf3667f2cac7
David Kraft <kraft153@umn.edu>
2018-07-29 12:36:13 -0500

    Built app

--- src/App.js
+++ src/App.js
@@ -2,36 +2,83 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.cards to the cards json array
   state = {
-    friends
+    cards,
+    message: "Hello World!",
+    score: 0,
+    hiScore: 0
   };
+
+  checkHiScore() {
+    if (this.state.score === 12) {
+      return this.state.score
+    } else if (this.state.hiScore === this.state.score){
+      return this.state.score + 1
+    } else {
+      return this.state.hiScore
+    }
+  }
 
   removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    // Filter this.state.cards for cards with an id not equal to the id being removed
+    const cards = this.state.cards.filter(friend => friend.id !== id);
+    // Set this.state.cards equal to the new cards array
+    this.setState({ cards });
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  handleClicked = (item) => {
+    let stateArray= this.state.cards.slice()
+  if (!item.clicked) {
+    for (let i=0; i<stateArray.length; i++) {
+      if (stateArray[i].id === item.id) {
+        stateArray[i].clicked = true
+      }
+    }
+    this.setState({
+      score: this.state.score + 1,
+      hiScore: this.checkHiScore(),
+      message: "You've got a point!",
+      cards: stateArray.sort(() => Math.random() - 0.5)
+    })
+
+
+  } else {
+    this.setState({
+      score: 0,
+      hiScore: this.checkHiScore(),
+      message: "That's already been clicked!",
+      cards: cards
+    });
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+
+  // Map over this.state.cards and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+        <Title>{this.state.message}</Title>
+        <Title>Score: {this.state.score}</Title>
+        <Title>High-Score: {this.state.hiScore}</Title>
+        {this.state.cards.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            clicked={friend.clicked}
+            handleClicked={this.handleClicked}
           />
         ))}
       </Wrapper>
